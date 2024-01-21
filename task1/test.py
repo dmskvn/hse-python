df15db1b2d8a46d2886a61dffd83838bccdb41d4
Moskvin Denis <you@example.com>
2024-01-21 19:59:45 +0300

    Add failed test

--- task1/test.py
+++ task1/test.py
@@ -7,7 +7,15 @@ def test(a,b):
 
     assert c == test_c
 
+def test_failed(a,b):
+    c = cal(a,b)
+
+    test_c = a+b
+
+    assert c == test_c+1
+
 
 test(10,20)
 test(1,15)
 test(40,150)
+test_failed(10,20)
